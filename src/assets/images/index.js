68a8812df0094fa920901b64f211ee90edb30ceb
Enz1n <enzodeleonb49@gmail.com>
2024-07-10 12:54:39 -0300

    add images

--- src/assets/images/index.js
+++ src/assets/images/index.js
@@ -1,8 +1,47 @@
 //// cofffees /////
+import numero1 from "./coffees/1.jpg";
+import numero1single from "./coffees/1single.jpg";
+import numero2 from "./coffees/2.jpg";
+import numero2single from "./coffees/2single.png";
+import numero3 from "./coffees/3.jpg";
+import numero3single from "./coffees/3single.jpg";
+import numero4 from "./coffees/4.jpg";
+import numero4single from "./coffees/4single.jpg";
+import numero5 from "./coffees/5.jpg";
+import numero5single from "./coffees/5single.jpg";
+import numero6 from "./coffees/6.jpg";
+import numero6single from "./coffees/6single.jpg";
+import numero7 from "./coffees/7.jpg";
+import numero7single from "./coffees/7single.jpg";
+import numero8 from "./coffees/8.jpg";
+import numero8single from "./coffees/8single.jpg";
+import numero9 from "./coffees/9.jpg";
+import numero9single from "./coffees/9single.jpg";
+import numero10 from "./coffees/10.jpg";
+import numero10single from "./coffees/10single.jpg";
+import numero11 from "./coffees/11.jpg";
+import numero11single from "./coffees/11single.jpg";
+import numero12 from "./coffees/12.jpg";
+import numero12single from "./coffees/12single.jpg";
+import numero13 from "./coffees/13.jpg";
+import numero13single from "./coffees/13single.jpg";
+import numero14 from "./coffees/14.jpg";
+import numero14single from "./coffees/14single.jpg";
+import numero15 from "./coffees/15.jpg";
+import numero15single from "./coffees/15single.jpg";
+import numero16 from "./coffees/16.jpg";
+import numero16single from "./coffees/16single.jpg";
+import numero17 from "./coffees/17.jpg";
+import numero17single from "./coffees/17single.jpg";
+import numero18 from "./coffees/18.jpg";
+import numero18single from "./coffees/18single.jpg";
+import numero19 from "./coffees/19.jpg";
+import numero19single from "./coffees/19single.jpg";
+import numero20 from "./coffees/20.jpg";
+import numero20single from "./coffees/20single.jpg";
 import dark1 from "./coffees/dark1.JPG";
 import dark1Single from "./coffees/dark1Single.JPG";
 import dark2 from "./coffees/dark2.JPG";
-import numero1 from "./coffees/numero1.JPG";
 import dark3 from "./coffees/dark3.JPG";
 import dark3Single from "./coffees/dark3Single.JPG";
 import decaf from "./coffees/decaf.JPG";
@@ -28,10 +67,49 @@ import vanilla from "./collections/vanilla.JPG";
 import irishCream from "./collections/irish cream.JPG";
 
 export {
+  numero1,
+  numero1single,
+  numero2,
+  numero2single,
+  numero3,
+  numero3single,
+  numero4,
+  numero4single,
+  numero5,
+  numero5single,
+  numero6,
+  numero6single,
+  numero7,
+  numero7single,
+  numero8,
+  numero8single,
+  numero9,
+  numero9single,
+  numero10,
+  numero10single,
+  numero11,
+  numero11single,
+  numero12,
+  numero12single,
+  numero13,
+  numero13single,
+  numero14,
+  numero14single,
+  numero15,
+  numero15single,
+  numero16,
+  numero16single,
+  numero17,
+  numero17single,
+  numero18,
+  numero18single,
+  numero19,
+  numero19single,
+  numero20,
+  numero20single,
   dark1,
   dark1Single,
   dark2,
-  numero1,
   dark3,
   dark3Single,
   decaf,
